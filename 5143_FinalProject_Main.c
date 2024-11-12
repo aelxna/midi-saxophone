495f96ba2066231677b97c280425fe4962decf8b
Lily <gahymes@gmail.com>
2024-11-12 10:33:21 -0600

    Created main file
    nothing else just created main file to make sure the compiler worked

---
+++ 5143_FinalProject_Main.c
@@ -0,0 +1,15 @@
+/*
+ * File:   5143_FinalProject_Main.c
+ * Author: gahym
+ *
+ * Created on November 12, 2024, 10:32 AM
+ */
+
+
+#include <avr/io.h>
+
+int main(void) {
+    /* Replace with your application code */
+    while (1) {
+    }
+}
